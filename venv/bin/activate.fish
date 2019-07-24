74ec9f09202a4897d7c75b598ef9ff88c011b40d
mnreyes1 <mnreyes1@uc.cl>
2019-07-23 21:15:09 -0400

    CAmbie la virtual env

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -56,7 +56,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/batcaver/Escritorio/Juego/venv"
+set -gx VIRTUAL_ENV "/home/batcaver/Escritorio/game/venv"
 
 # https://github.com/fish-shell/fish-shell/issues/436 altered PATH handling
 if test (echo $FISH_VERSION | tr "." "\n")[1] -lt 3
--- venv/bin/activate.xsh
+++ venv/bin/activate.xsh
@@ -28,7 +28,7 @@ def _deactivate(args):
 _deactivate(["nondestructive"])
 aliases["deactivate"] = _deactivate
 
-$VIRTUAL_ENV = r"/home/batcaver/Escritorio/Juego/venv"
+$VIRTUAL_ENV = r"/home/batcaver/Escritorio/game/venv"
 
 $_OLD_VIRTUAL_PATH = $PATH
 $PATH = $PATH[:]
